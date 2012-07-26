d645956f57b2031819736e97fed21aebc5472455
Vangelis Bibakis <bibakisv@gmail.com>
2012-07-27 02:42:58 +0300

    Finalized the documentation layout

--- docs/mainframe/nav/nav.js
+++ docs/mainframe/nav/nav.js
@@ -12,58 +12,47 @@ function create_menu(basepath)
 		'<td class="td" valign="top">' +
 
 		'<ul>' +
-//		'<li><a href="'+base+'sample/sample.html">Sample</a></li>' +
+		'<li><a href="'+base+'index.html">User guide home</a></li>' +
+		'<li><a href="'+base+'toc.html">Table of contents</a></li>' +
 		'</ul>' +
 
 		'<h3>Basic Info</h3>' +
 		'<ul>' +
-//		'<li><a href="'+base+'sample/sample.html">Sample</a></li>' +
+		'<li><a href="'+base+'basic/introduction.html">Introduction</a></li>' +
+		'<li><a href="'+base+'basic/structure.html">File structure</a></li>' +
+		'<li><a href="'+base+'basic/installation.html">Installation</a></li>' +
+		'<li><a href="'+base+'basic/first_project.html">Your first project</a></li>' +
+		'<li><a href="'+base+'basic/learning.html">Learning Mainframe</a></li>' +
 		'</ul>' +
 
-		'<h3>Installation</h3>' +
-		'<ul>' +
-//		'<li><a href="'+base+'sample/sample.html">Sample</a></li>' +
-		'</ul>' +
-
-		'<h3>Introduction</h3>' +
-		'<ul>' +
-//		'<li><a href="'+base+'sample/sample.html">Sample</a></li>' +
-		'</ul>' +
-		
-		'<h3>Tutorial</h3>' +
-		'<ul>' +
-//		'<li><a href="'+base+'sample/sample.html">Sample</a></li>' +
-		'</ul>' +
-		
 		'</td><td class="td_sep" valign="top">' +
 
 		'<h3>General Topics</h3>' +
 		'<ul>' +
-//		'<li><a href="'+base+'sample/sample.html">Sample</a></li>' +
+		'<li><a href="'+base+'general/libs.html">3rd party libraries</a></li>' +
+		'<li><a href="'+base+'general/themes.html">Themes</a></li>' +
+		'<li><a href="'+base+'general/assets.html">Assets</a></li>' +
+		'<li><a href="'+base+'general/plugins.html">Plugins</a></li>' +
 		'</ul>' +
+
+		
+
+		'</td><td class="td_sep" valign="top">' +
 
 		'<h3>Additional Resources</h3>' +
 		'<ul>' +
-//		'<li><a href="'+base+'sample/sample.html">Sample</a></li>' +
+		'<li><a href="http://mainframephp.com">Mainframe PHP home page</a></li>' +
+		'<li><a href="http://demianlabs.com">Demian Labs</a></li>' +
 		'</ul>' +
 
 		'</td><td class="td_sep" valign="top">' +
 
-		'<h3>Class Reference</h3>' +
-		'<ul>' +
-//		'<li><a href="'+base+'sample/sample.html">Sample</a></li>' +
-		'</ul>' +
-
-		'</td><td class="td_sep" valign="top">' +
-
-		'<h3>Driver Reference</h3>' +
-		'<ul>' +
-//		'<li><a href="'+base+'sample/sample.html">Sample</a></li>' +
 		'</ul>' +
 
 		'<h3>Helper Reference</h3>' +
 		'<ul>' +
-//		'<li><a href="'+base+'sample/sample.html">Sample</a></li>' +
+		'<li><a href="'+base+'helpers/debug_helper.html">Debug helper</a></li>' +
+		'<li><a href="'+base+'helpers/mainframe_helper.html">Mainframe helper</a></li>' +
 		'</ul>' +
 
 		'</td></tr></table>');
--- libs/common/helpers/mainframe_helper.php
+++ libs/common/helpers/mainframe_helper.php
@@ -33,3 +33,109 @@ function active_class($segment_value, $return = FALSE){
 }
 
  
+
+
+/*
+ * Returns a config item
+*/
+function config($item){
+	$ci =& get_instance();
+	return $ci->config->item($item);
+}
+
+
+/*
+ * Returns a session item
+*/
+function session($item){
+	$ci =& get_instance();
+	return $ci->session->userdata($item);
+}
+
+
+function validate($rules = array()){
+	$ci =& get_instance();
+
+	// If there are no rules set for the validation then just check
+	// if there was a form submited or not
+	if (count($rules) == 0){
+		if (count($_POST) > 0){
+			return TRUE;
+		}
+		else {
+			return FALSE;
+		}
+	}
+
+	$ci->load->library('form_validation');
+
+	foreach ($rules as $rule){
+		$ci->form_validation->set_rules($rule, $rule, 'required');
+	}
+
+	return $ci->form_validation->run();
+}
+
+
+function save_form($table, $where = array(), $datefields = array()){
+	$ci =& get_instance();
+	$db_data = array();
+
+	foreach ($_POST as $field => $value){
+		if ($field != 'submit'){
+			if (in_array($field, $datefields)){
+				$db_data[$field] = input_gr_to_mysql($value);
+			}
+			else {
+				$db_data[$field] = $value;
+			}
+		}
+	}
+
+	if (count($where) > 0){
+		$ci->db->where($where);
+		$ci->db->update($table, $db_data);
+	}
+	else {
+		$ci->db->insert($table, $db_data);
+	}
+}
+
+
+function json($response = array()){
+	echo json_encode($response);
+	die();
+}
+
+
+function google_analytics(){
+	$ci = &get_instance();
+	if (((base_url() === 'http://deskhot.com/') || (base_url() === 'http://m.deskhot.com/'))
+			&& ($ci->session->userdata('username') != 'bibakisv') && ($ci->session->userdata('username') != 'vskandalakis')){
+		$output = '
+		<script type="text/javascript">
+
+		var _gaq = _gaq || [];
+		_gaq.push([\'_setAccount\', \'UA-8249283-14\']);
+		_gaq.push([\'_setDomainName\', \'.deskhot.com\']);
+		_gaq.push([\'_trackPageview\']);
+
+		(function() {
+		var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
+		ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
+		var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
+	</script>
+	';
+		return $output;
+	}
+}
+
+
+// returns a suffix for css and js files to avoid caching. Example: ?v=15
+function cjsuf(){
+	$ci = & get_instance();
+	$cjsuf = '?v='.$ci->config->item('cjsuf');
+	return $cjsuf;
+}
